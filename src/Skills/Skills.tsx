2b9dadcbd37f670021658c76561bca6f790567ad
Anton Stanev <anton.stanev@eyasgaming.com>
2023-11-09 11:38:09 +0200

    add section to skills

--- src/Skills/Skills.tsx
+++ src/Skills/Skills.tsx
@@ -8,38 +8,41 @@ import CypressLogo from '../images/skills/cypress-logo.png';
 import SQLLogo from '../images/skills/sql-database.png';
 import DataScienceLogo from '../images/skills/data-science-logo.png';
 import './Skills.scss';
+import Section from '../Section/Section';
 
 const Skills: FC = () => {
   return (
-    <div className='skills'>
-      <h2>Skills</h2>
-      <div className='skills__list'>
-        <div className='skills__list-item'>
-          <img src={AEMLogo} alt='Adobe Experience Manager logo' />
-        </div>
-        <div className='skills__list-item'>
-          <img src={ReactLogo} alt='React logo' />
-        </div>
-        <div className='skills__list-item'>
-          <img src={TypeScriptLogo} alt='Type Script logo' />
-        </div>
-        <div className='skills__list-item'>
-          <img src={JavaScriptLogo} alt='Java Script logo' />
-        </div>
-        <div className='skills__list-item'>
-          <img src={ReduxLogo} alt='Redux logo' />
-        </div>
-        <div className='skills__list-item'>
-          <img src={CypressLogo} alt='Cypress logo' />
-        </div>
-        <div className='skills__list-item'>
-          <img src={SQLLogo} alt='SQL logo' />
-        </div>
-        <div className='skills__list-item'>
-          <img src={DataScienceLogo} alt='Data Science logo' />
+    <Section>
+      <div className='skills'>
+        <h2>Skills</h2>
+        <div className='skills__list'>
+          <div className='skills__list-item'>
+            <img src={AEMLogo} alt='Adobe Experience Manager logo' />
+          </div>
+          <div className='skills__list-item'>
+            <img src={ReactLogo} alt='React logo' />
+          </div>
+          <div className='skills__list-item'>
+            <img src={TypeScriptLogo} alt='Type Script logo' />
+          </div>
+          <div className='skills__list-item'>
+            <img src={JavaScriptLogo} alt='Java Script logo' />
+          </div>
+          <div className='skills__list-item'>
+            <img src={ReduxLogo} alt='Redux logo' />
+          </div>
+          <div className='skills__list-item'>
+            <img src={CypressLogo} alt='Cypress logo' />
+          </div>
+          <div className='skills__list-item'>
+            <img src={SQLLogo} alt='SQL logo' />
+          </div>
+          <div className='skills__list-item'>
+            <img src={DataScienceLogo} alt='Data Science logo' />
+          </div>
         </div>
       </div>
-    </div>
+    </Section>
   );
 };
 export default memo(Skills);
